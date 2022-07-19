5dac37653b8bcd491ae88a8322b7e6a344fd611e
[digvijay khairnar] <[1digvijaykhairnar@gmail.com]>
2022-07-19 22:32:26 +0530

    Initial commit

---
+++ src/main/java/com/application/courselibrary/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.application.courselibrary.repository;
+
+import com.application.courselibrary.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author,Long> {
+
+}
